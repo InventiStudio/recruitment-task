0f98349d05eff37cdfb74e2732a3c92c38576ebd
Kamil Borkowski <kamil.borkowski.02@gmail.com>
2018-04-04 22:08:58 +0200

    Add auth store module

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,14 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 import createLogger from 'vuex/dist/logger'
+import auth from './modules/auth'
 
 Vue.use(Vuex)
 
 const isDebug = process.env.NODE_ENV !== 'production' && process.env.NODE_ENV !== 'testing'
-const modules = {}
+const modules = {
+  auth,
+}
 
 export default new Vuex.Store({
   plugins: isDebug
---
+++ src/store/modules/auth.js
@@ -0,0 +1,46 @@
+import ls from 'local-storage'
+import store from 'src/store'
+import router from 'src/router'
+import { STORAGE_AUTH_TOKEN } from 'src/constants'
+import api from 'src/services/api'
+
+const state = {
+  isLoggedIn: !!ls.get(STORAGE_AUTH_TOKEN),
+}
+
+const getters = {
+  isLoggedIn: state => state.isLoggedIn,
+}
+
+const mutations = {
+  LOGIN(state) {
+    state.isLoggedIn = true
+  },
+}
+
+const actions = {
+  async login({ commit }, { email, password, lsToken }) {
+    try {
+      const { headers: { 'x-auth-token': apiToken } } = lsToken
+        ? await api.get(`/sessions/${lsToken}`)
+        : await api.post('/sessions', { email, password })
+      ls.set(STORAGE_AUTH_TOKEN, apiToken)
+      api.setHeader('x-auth-token', apiToken)
+      if (!lsToken) {
+        commit('LOGIN')
+        // router.push({ name: 'Wallet' })
+      }
+    } catch (err) {
+      if (lsToken) ls.remove(STORAGE_AUTH_TOKEN)
+      throw err
+    }
+  },
+}
+
+export default {
+    namespaced: 'auth',
+    state,
+    getters,
+    mutations,
+    actions,
+}
