d9697a79c57c59eda68180aac01e99e061b489b9
Kamil Borkowski <kamil.borkowski.02@gmail.com>
2018-04-04 22:08:46 +0200

    Change API path

--- src/config.js
+++ src/config.js
@@ -2,7 +2,7 @@ const env = process.env.NODE_ENV
 
 const config = {
   development: {
-    API_URL: 'localhost:4000',
+    API_URL: 'http://217.182.66.13:4000/',
   },
   production: {
     API_URL: '',
