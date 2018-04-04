86431124a31d954af0d0330e8a15e2a3e6adbd62
Kamil Borkowski <kamil.borkowski.02@gmail.com>
2018-04-04 22:37:35 +0200

    Add style objects, improve styling of SignIn view

--- src/views/SignIn/index.vue
+++ src/views/SignIn/index.vue
@@ -1,23 +1,24 @@
 <template lang="pug">
-  main
+  main.sign-in
     .container
       .row.justify-content-center
-        .col-12.col-md-10
-          img(src="~assets/logo.svg", alt="Wallet: InventiStudio recruitment task")
+        .col-12.text-center
+          img.sign-in__logo.d-inline-block(src="~assets/logo.svg", alt="Wallet: InventiStudio recruitment task")
+        .col-12.col-md-7.col-lg-5.col-xl-4
           form
-            label.fs-11 E-mail address
-            input(
+            label.fs-11.mb-8 E-mail address
+            input.o-input.mb-16(
               v-model.trim="email",
               type="email",
               placeholder="john.doe@mail.com",
             )
-            label.fs-11 Password
-            input(
+            label.fs-11.mb-8 Password
+            input.o-input.mb-16(
               v-model.trim="password",
               type="password",
               placeholder="Your pass****",
             )
-          button.bg-blue(
+          button.o-btn.bg-blue.mt-40.w-100(
             @click="signIn()",
             :disabled="$v.$error",
             type="button",
@@ -26,6 +27,7 @@
 </template>
 
 <script>
+  import { mapActions } from 'vuex'
   import { required, email } from 'vuelidate/lib/validators'
 
   export default {
@@ -35,9 +37,13 @@
       password: '',
     }),
     methods: {
+      ...mapActions({
+        login: 'auth/login',
+      }),
       async signIn() {
         try {
           if (this.$v.$touch() || this.$v.$error) return null
+          await this.login({ email: this.email, password: this.password })
         } catch (err) {
           throw err
         }
@@ -49,3 +55,11 @@
     },
   }
 </script>
+
+<style lang="sass" scoped>
+  .sign-in
+    padding-top: 100px
+
+  .sign-in__logo
+    margin-bottom: 110px
+</style>
--- test/e2e/specs/test.js
+++ test/e2e/specs/test.js
@@ -3,7 +3,6 @@ describe('Test', () => {
     browser
       .url(browser.globals.devServerURL)
       .waitForElementVisible('#app', 5000)
-      .assert.containsText('h1', 'Hello from your awesome Vue.js project!')
       .assert.elementCount('img', 1)
       .end()
   })
